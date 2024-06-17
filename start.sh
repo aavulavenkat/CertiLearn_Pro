d78212eeb9bb403c532f77682710f359e5243be8
Aavula Venkateshwar <128135912+aavulavenkat@users.noreply.github.com>
2024-06-17 19:04:56 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ensure Python dependencies are installed
+pip install -r requirements.txt
+
+# Set environment variables (if needed)
+# export TELEGRAM_BOT_TOKEN='your-telegram-bot-token'
+
+# Start your Telegram bot
+python Certilearnpro.py
